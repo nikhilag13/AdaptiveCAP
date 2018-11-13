b11aafdfbef381441f14600a04bb33d136999fcf
dgaonkar17 <dgaonkar17@gmail.com>
2018-11-12 20:49:56 -0800

    Proto file updated

---
+++ gRPC/src/main/java/org/sjsu/grpcCommunicator/InitScript.java
@@ -0,0 +1,79 @@
+package org.sjsu.grpcCommunicator;
+
+import com.mongodb.*;
+import org.apache.log4j.Logger;
+import java.util.*;
+import java.lang.*;
+
+public class InitScript {
+    final static Logger logger = Logger.getLogger(InitScript.class.getName());
+
+    class myRunnable implements Runnable {
+        int id ;
+
+        myRunnable(int id) {
+            this.id =id;
+        }
+
+
+        public void run() {
+            System.out.println("Run method "+String.valueOf(id));
+            logger.info("Run method "+String.valueOf(id));
+            spawnNode(id);
+        }
+    }
+
+    public void spawn() {
+        for ( int i = 0; i < 8; i++) {
+
+            Thread thread = new Thread(new myRunnable(i));
+            thread.start();
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        InitScript initScript = new InitScript();
+        initScript.cleanDB();
+        initScript.buildDB();
+         initScript.spawn();
+       // initScript.spawnNode(0);
+
+
+
+    }
+
+    public  void cleanDB() {
+        try {
+            MongoClient mongo = new MongoClient("localhost", 27017);
+            DB db = mongo.getDB("cmpe295Project");
+            DBCollection collection = db.getCollection("spanningtree");
+            BasicDBObject document = new BasicDBObject();
+
+            // Delete All documents from collection Using blank BasicDBObject
+            collection.remove(document);
+            logger.info("Cleaning DB if it exists");
+        } catch (Exception e) {
+            logger.error(e);
+//            logger.error(traceback.format_exc());
+        }
+
+    }
+
+    public  void buildDB() {
+        logger.info("Creating New DB");
+        //make file to populate a tree
+        InsertSpanningTree.insertNodes();
+    }
+
+    public  void spawnNode( int id) {
+        System.out.println("Spwanning new node "+String.valueOf(id));
+        logger.info("Spwanning new node "+String.valueOf(id));
+        Node node = new Node(id);
+        CommunicatorServer cms = new CommunicatorServer();
+        cms.serve(node);
+    }
+
+
+}
---
+++ gRPC/src/main/java/org/sjsu/grpcCommunicator/NodeIdsList.java
@@ -0,0 +1,36 @@
+package org.sjsu.grpcCommunicator;
+
+import java.util.HashMap;
+
+public class NodeIdsList {
+    private HashMap<String, String> nodeIdsList = new HashMap<String,String>();
+
+     public  NodeIdsList(){
+         nodeIdsList.put("0","localhost:50049");
+         nodeIdsList.put("1","localhost:50050");
+         nodeIdsList.put("2","localhost:50051");
+         nodeIdsList.put("3","localhost:50052");
+         nodeIdsList.put("4","localhost:50053");
+         nodeIdsList.put("5","localhost:50054");
+         nodeIdsList.put("6","localhost:50055");
+         nodeIdsList.put("7","localhost:50056");
+         nodeIdsList.put("8","localhost:50057");
+         nodeIdsList.put("9","localhost:50058");
+         nodeIdsList.put("10","localhost:50059");
+         nodeIdsList.put("11","localhost:50060");
+         nodeIdsList.put("12","localhost:50061");
+         nodeIdsList.put("13","localhost:50062");
+         nodeIdsList.put("14","localhost:50063");
+         nodeIdsList.put("15","localhost:50064");
+         nodeIdsList.put("16","localhost:50065");
+         nodeIdsList.put("17","localhost:50066");
+         nodeIdsList.put("18","localhost:50067");
+         nodeIdsList.put("19","localhost:50068");
+         nodeIdsList.put("20","localhost:50069");
+         nodeIdsList.put("21","localhost:50070");
+     }
+
+    public HashMap<String, String> getNodeIdsList() {
+        return nodeIdsList;
+    }
+}
